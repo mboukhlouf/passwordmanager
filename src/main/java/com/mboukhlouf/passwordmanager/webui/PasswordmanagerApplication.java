bbe3ebf4f4096293f6ae1e4b282a566a63e7bdb1
Mohammed Boukhlouf <mohammed.boukhlouf@outlook.com>
2020-09-12 14:45:26 +0100

    Initial commit

---
+++ src/main/java/com/mboukhlouf/passwordmanager/webui/PasswordmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.mboukhlouf.passwordmanager.webui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mboukhlouf/passwordmanager/PasswordmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mboukhlouf.passwordmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
