2282460e23f80a37d9570cef1fe0373fdfab7909
Mohammed Boukhlouf <mohammed.boukhlouf@outlook.com>
2020-09-14 19:52:59 +0100

    Views

--- src/main/java/com/mboukhlouf/passwordmanager/webui/controllers/PasswordsController.java
+++ src/main/java/com/mboukhlouf/passwordmanager/webui/controllers/PasswordsController.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -33,6 +35,7 @@ public class PasswordsController {
         }
         Iterable<UserPasswordDto> passwords = mediator.send(new GetUserPasswordsQuery());
         model.addAttribute("username", currentUserService.getUsername());
+        model.addAttribute("passwords", passwords);
         return "passwords/index";
     }
 
@@ -41,22 +44,33 @@ public class PasswordsController {
         if(!currentUserService.isAuthenticated()) {
             return "redirect:/authentication/login";
         }
-
-        AddPasswordCommand command = new AddPasswordCommand("label", "account", "password", "url", "tags");
-        Integer passwordId = mediator.send(command);
         model.addAttribute("username", currentUserService.getUsername());
         return "passwords/add";
     }
 
+    @RequestMapping(value = {"add"}, method = RequestMethod.POST)
+    public String addPost(@RequestParam String label, 
+    @RequestParam String account,
+    @RequestParam String password,
+    @RequestParam String url,
+    @RequestParam String tags) throws Exception {
+        if(!currentUserService.isAuthenticated()) {
+            return "redirect:/authentication/login";
+        }
+        
+        AddPasswordCommand command = new AddPasswordCommand(label, account, password, url, tags);
+        mediator.send(command);
+        return "redirect:/passwords";
+    }
+
     @RequestMapping(value = {"delete"})
-    @ResponseBody
-    public String deletePost() throws Exception {
+    public String deletePost(@RequestParam Integer id) throws Exception {
         if(!currentUserService.isAuthenticated()) {
             return "redirect:/authentication/login";
         }
 
-        DeletePasswordCommand command = new DeletePasswordCommand(8);
-        Integer passwordId = mediator.send(command);
-        return "passwordId";
+        DeletePasswordCommand command = new DeletePasswordCommand(id);
+        mediator.send(command);
+        return "redirect:/passwords/";
     }
 }
